785ef0573a959ff7f1668d39757c7eb0da492ebd
Gladies <49598326+gladieschanggoodluck@users.noreply.github.com>
2021-05-03 20:28:38 -0700

    Update utils.py

--- VA plant/utils.py
+++ VA plant/utils.py
@@ -5,11 +5,11 @@ Created on Mon Apr 26 14:45:12 2021
 @author: gladi
 """
 
-# Utils for preprocessing data etc 
+# Utils for preprocessing data  
 import tensorflow as tf
 import googleapiclient.discovery
 from google.api_core.client_options import ClientOptions
-
+# The five classes of VA plants study
 base_classes = ['Arrowhead', 'Duckweed', 'Grassy Mud Plaintain', 'Hydrilla', 'Watercress']
 
 
@@ -35,6 +35,7 @@ classes_and_models = {
     }
 }
 
+## define predict_json 
 def predict_json(project, region, model, instances, version=None):
     """Send json data to a deployed model for prediction.
     Args:
@@ -49,7 +50,7 @@ def predict_json(project, region, model, instances, version=None):
         Mapping[str: any]: dictionary of prediction results defined by the 
             model.
     """
-    # Create the ML Engine service object
+    # Define the ML Engine service object
     prefix = "{}-ml".format(region) if region else "ml"
     api_endpoint = "https://{}.googleapis.com".format(prefix)
     client_options = ClientOptions(api_endpoint=api_endpoint)
@@ -59,7 +60,7 @@ def predict_json(project, region, model, instances, version=None):
     if version is not None:
         model_path += "/versions/{}".format(version)
 
-    # Create ML engine resource endpoint and input data
+    # Define ML engine resource endpoint and input data
     ml_resource = googleapiclient.discovery.build(
         "ml", "v1", cache_discovery=False, client_options=client_options).projects()
     instances_list = instances.numpy().tolist() # turn input into list (ML Engine wants JSON)
@@ -80,7 +81,7 @@ def predict_json(project, region, model, instances, version=None):
 
     return response["predictions"]
 
-# Create a function to import an image and resize it to be able to be used with our model
+# Define a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, rescale=False):
   """
   Reads in an image from filename, turns it into a tensor and reshapes into
